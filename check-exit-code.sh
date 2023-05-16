9adfebb4c2cb8a6f442ec3777799668b88462337
Engr. Peter Goteh <edwardpeter547@gmail.com>
2023-05-16 01:00:23 +0000

    Demostrated the use of exit codes

--- check-exit-code.sh
+++ check-exit-code.sh
@@ -2,12 +2,12 @@
 
 package=noexist
 
-sudo apt-get update && sudo apt-get install $package -y
+sudo apt-get install $package -y >>log/package_install_result.log
 
 if [ $? -eq 0 ]; then
     echo "The installation of $package was successful."
     echo "The new command is available here:"
     which $package
 else
-    echo "Unable to install $package"
+    echo "Unable to install $package" >>log/package_install_failure.log
 fi
